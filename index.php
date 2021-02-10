74e5434a3a9a7443c70f425909eae4162d49fc75
nematu <nayaz-akhundzada@mail.greenriver.edu>
2021-02-10 01:36:40 -0500

    modified order form so it uses array to pick data, included <repeat> in per-order.html

--- index.php
+++ index.php
@@ -1,63 +1,78 @@
 <?php
-//Turn on error reporting
-ini_set('display_errors',1);
+// Turn on error reporting
+ini_set('display_errors', 1);
 error_reporting(E_ALL);
 
-//start session
+// Start a session
 session_start();
 
-//require the autoload file
-require_once ('vendor/autoload.php');
+//Require the auto autoload file
+require_once('vendor/autoload.php');
+require_once ('model/data-layer.php');
 
-//create an instance of th Base class
+//Create an instance of the Base class
 $f3 = Base::instance();
+$f3->set('Debug',3);
 
-//set a debug for fat free
-$f3 -> set('DEBUG',3);
+//Define a default route (home page)
+$f3->route('GET /', function() {
 
-// define a default route
-$f3 -> route('GET /', function ()
-{
     $view = new Template();
-    echo $view -> render('views/pet-home.html');
-}
-);
+    echo $view->render('views/pet-home.html');
+});
 
-//set a route for order
-$f3 -> route('GET /order', function ()
-{
-   // echo " My Food Page";
-    $view = new Template();
-    echo $view -> render('views/pet-order.html');
-}
-);
 
-//set a route for order2
-$f3 -> route('POST /order2', function ()
-{
-    //var_dump($_POST);
-    if(isset($_POST['pet'])) {
-        $_SESSION['pet'] = $_POST['pet'];
+$f3->route('GET|POST /order', function($f3) {
+    if($_SERVER['REQUEST_METHOD'] == 'POST') {
+        //Validate the data
+        if (empty($_POST['typeOfPet'])) {
+
+        } else {
+
+        }
     }
 
-    if(isset($_POST['color'])) {
+    //if it doesnt work with conditional
+    $f3->set('colors', getColors());
+
+    $view = new Template();
+    echo $view->render('views/pet-order.html');
+
+});
+
+$f3->route('GET|POST /order2', function($f3){
+    if(isset($_POST['typePet'])){
+        $_SESSION['typePet'] = $_POST['typePet'];
+    }
+    if(isset($_POST['color'])){
         $_SESSION['color'] = $_POST['color'];
     }
-    $view = new Template();
-    echo $view -> render('views/pet-order2.html');
-}
-);
 
-//summary route
-$f3 -> route ('POST /summary', function ()
-{
-   if (isset($_POST['petName'])) {
-       $_SESSION['petName'] = $_POST['petName'];
-   }
+    $f3 ->set('sizes', getSizes());
+    $f3 ->set('accessories', getAccessories());
 
-    //Display a view
+    //display a view
     $view = new Template();
-    echo $view->render('views/order-summary.html');
+    echo $view->render('views/pet-order2.html');
 });
-//run fat free
-$f3 -> run();
+
+//Define a summary route
+$f3->route('POST /summary', function() {
+
+    if(isset($_POST['size'])){
+        $_SESSION['size'] = $_POST['size'];
+    }
+    if(isset($_POST['accessory'])){
+        $_SESSION['accessory'] = $_POST['accessory'];
+    }
+
+    if (isset($_POST['name'])) {
+        $_SESSION['name'] = $_POST['name'];
+    }
+
+    
+    $view = new Template();
+    echo $view->render("views/order-summary.html");
+});
+//Run fat free
+$f3->run();
