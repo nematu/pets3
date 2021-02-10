72c694024b8efd16a39689c612915c6c860e2ac3
nematu <nayaz-akhundzada@mail.greenriver.edu>
2021-02-10 01:12:39 -0500

    added data-layer.php

---
+++ model/data-layer.php
@@ -0,0 +1,19 @@
+<?php
+/* return a list of colors
+* @return array
+ *
+ */
+function getColors()
+{
+    return $colors = array("red","blue","orange","purple");
+
+}
+
+function getSizes() {
+    return $sizes = array("small", "medium", "large");
+
+}
+
+function getAccessories() {
+    return $accessories = array("bow", "shoes", "rain jacket", "top hat", "earrings", "saddle");
+}
